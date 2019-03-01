30f8dd1603ea599945db57c577586963bda1b2c6
phacks <nicolas.goutay@gmail.com>
2019-03-01 15:56:42 +0100

    Redirect to /projects after successful login

--- frontend/src/redux/login/sagas.ts
+++ frontend/src/redux/login/sagas.ts
@@ -1,3 +1,4 @@
+import { push } from 'connected-react-router';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { ActionType, getType } from 'typesafe-actions';
 
@@ -11,6 +12,7 @@ export function* loginUser(action: ActionType<typeof loginUserRequest>) {
     const token: string | undefined = yield call(login, endpoint, action.payload);
     if (token) {
       yield put(loginUserSuccess({ token }));
+      yield put(push('/projects'));
     } else {
       yield put(loginUserError({ errorMessage: 'No token in login response body' }));
     }
