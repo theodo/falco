10167e4ec1dee0065439b3b8cc797c93ea06dc72
phacks <nicolas.goutay@gmail.com>
2019-03-01 18:07:18 +0100

    Finish styling header

--- frontend/src/components/Root/Root.style.ts
+++ frontend/src/components/Root/Root.style.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 const StyledRoot = {
   Container: styled.div`
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
     height: 100%;
     background-color: #f0f4fe;
     font-family: 'IBM Plex Sans', sans-serif;
@@ -11,8 +15,10 @@ const StyledRoot = {
     background-color: #374894;
     color: #f8f9fa;
     display: flex;
+    justify-content: space-between;
     height: 70px;
     padding: 0 20px;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
   `,
   Logo: styled.div`
     display: flex;
@@ -26,6 +32,26 @@ const StyledRoot = {
       letter-spacing: 2px;
       font-weight: 600;
       font-size: 24px;
+    }
+  `,
+  Nav: styled.nav`
+    ul {
+      display: flex;
+      justify-content: center;
+      flex-direction: row;
+      padding: 0;
+      margin: 0;
+    }
+    li {
+      list-style: none;
+      margin-left: 20px;
+      line-height: 70px;
+      font-weight: 600;
+      letter-spacing: 1px;
+      box-shadow: inset 0 -6px #f8f9fa;
+    }
+    li:first-of-type {
+      margin-left: 0;
     }
   `,
 };
--- frontend/src/components/Root/Root.tsx
+++ frontend/src/components/Root/Root.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
+import { Link } from 'react-router-dom';
 
 import { flattenMessages } from 'services/i18n/intl';
 import enMessages from 'translations/en.json';
@@ -25,10 +26,21 @@ const Root: React.SFC<Props> = ({ children }) => (
   <IntlProvider locale="fr" messages={locales.fr}>
     <Style.Container>
       <Style.Header>
-        <Style.Logo>
-          <img src={logo} alt="Falco logo" />
-          <h1>FALCO</h1>
-        </Style.Logo>
+        <Link key={'logo'} to={`/projects`}>
+          <Style.Logo>
+            <img src={logo} alt="Falco logo" />
+            <h1>FALCO</h1>
+          </Style.Logo>
+        </Link>
+        <Style.Nav>
+          <ul>
+            <li>
+              <Link key={'navProjects'} to={`/projects`}>
+                MES PROJETS
+              </Link>
+            </li>
+          </ul>
+        </Style.Nav>
       </Style.Header>
       {children}
     </Style.Container>
