6be898481462275604bc5ea5a361995921301302
phacks <nicolas.goutay@gmail.com>
2019-03-09 22:42:48 +0100

    Send parameters along with project

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,4 +1,4 @@
-from projects.models import Page, Project
+from projects.models import Page, Project, ProjectAuditParameters
 from rest_framework import serializers
 
 
@@ -8,9 +8,16 @@ class PageSerializer(serializers.ModelSerializer):
         fields = ("uuid", "name", "url", "audits")
 
 
+class ProjectAuditParametersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectAuditParameters
+        fields = ("uuid", "location", "browser", "network_shape")
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     pages = PageSerializer(many=True)
+    audit_parameters_list = ProjectAuditParametersSerializer(many=True)
 
     class Meta:
         model = Project
-        fields = ("uuid", "name", "pages")
+        fields = ("uuid", "name", "pages", "audit_parameters_list")
