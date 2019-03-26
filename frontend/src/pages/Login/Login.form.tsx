3a6a09c74b5308e6a3f54bc387c4e24db55e01e8
phacks <nicolas.goutay@gmail.com>
2019-03-26 11:10:52 +0100

    Small design tweaks for Login page

--- frontend/src/pages/Login/Login.form.tsx
+++ frontend/src/pages/Login/Login.form.tsx
@@ -1,13 +1,10 @@
 import Button from '@material-ui/core/Button';
-import {
-  Field,
-  Form,
-  InjectedFormikProps,
-} from 'formik';
+import { Field, Form, InjectedFormikProps } from 'formik';
 import React, { CSSProperties } from 'react';
 
 import Input from 'components/Input';
 
+import Styles from './Login.style';
 import { FormValues } from './service';
 
 interface InnerLoginFormProps {
@@ -27,61 +24,38 @@ interface InnerLoginFormProps {
 const InnerLoginForm: React.SFC<InjectedFormikProps<InnerLoginFormProps, FormValues>> = props => {
   const { errors, touched, isSubmitting, token } = props;
 
-  const styles: Record<string, CSSProperties> = {
-    message: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    },
-    token: {
-      maxWidth: '400px',
-      wordBreak: 'break-all',
-      whiteSpace: 'pre-wrap'
-    }
-  };
-
   return (
-    <Form>
-      <div>
-        <Field
-          type="text"
-          name="username"
-          label="Username"
-          component={Input}
-          error={touched.username && errors.username}
-        />
-      </div>
-      <div>
-        <Field
-          type="password"
-          name="password"
-          label="Password"
-          component={Input}
-          error={touched.password && errors.password}
-        />
-      </div>
-      <Button
-        type="submit"
-        color="primary"
-        size="medium"
-        disabled={isSubmitting}
-        variant="outlined"
-      >
-        Connect
-      </Button>
-        {!token
-          ? null
-          : <div style={styles.message}>
-            <span>
-              Félicitations, vous êtes bien connectés avec le token :
-            </span>
-            <br/>
-            <pre style={styles.token}>
-              {token}
-            </pre>
-          </div>
-        }
-    </Form>
+    <Styles.Container>
+      <Form>
+        <div>
+          <Field
+            type="text"
+            name="username"
+            label="Username"
+            component={Input}
+            error={touched.username && errors.username}
+          />
+        </div>
+        <div>
+          <Field
+            type="password"
+            name="password"
+            label="Password"
+            component={Input}
+            error={touched.password && errors.password}
+          />
+        </div>
+        <Button
+          type="submit"
+          color="primary"
+          size="medium"
+          disabled={isSubmitting}
+          variant="outlined"
+        >
+          Connect
+        </Button>
+      </Form>
+    </Styles.Container>
   );
 };
 
---
+++ frontend/src/pages/Login/Login.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Styles = {
+  Container: styled.div`
+    margin: 100px auto 0;
+    width: 400px;
+    text-align: center;
+  `,
+};
+
+export default Styles;
