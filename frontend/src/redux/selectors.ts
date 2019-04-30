3d5dc138663b923e5ed50a7aca7c836e48524696
Etienne Grall <etienne.grall@vestiairecollective.com>
2019-04-30 14:34:56 +0200

    Use regex in selector

--- frontend/src/redux/selectors.ts
+++ frontend/src/redux/selectors.ts
@@ -2,8 +2,9 @@ import { RootStateWithRouter } from 'redux/types';
 
 export const getCurrentProject = (state: RootStateWithRouter) => {
   const url = state.router.location.pathname;
-  if (url) {
-    return state.projects.byId[url.split('/')[2]];
+  const match = url.match(/project\/([a-zA-Z0-9\-]+)\//);
+  if (match) {
+    return state.projects.byId[match[1]];
   }
   return;
 };
