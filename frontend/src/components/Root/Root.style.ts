6dad62076ecbfde94f012ac91b9a7dc62371f425
Vincent Larrat <vincentla@theodo.fr>
2019-05-08 11:44:24 +0200

    [101] Hide left menu on login page

--- frontend/src/components/Root/Root.style.ts
+++ frontend/src/components/Root/Root.style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { fontFamily, getSpacing } from 'stylesheet';
 
+interface ContentProps {
+  isLoginPage: boolean;
+}
+
 const StyledRoot = {
   Page: styled.div`
     height: 100%;
@@ -12,11 +16,11 @@ const StyledRoot = {
   Content: styled.div`
     position: relative;
     top: 100px;
-    left: 490px;
+    left: ${(props: ContentProps) => (props.isLoginPage ? '0' : '490px')};
     margin-left: ${getSpacing(8)};
     padding-right: ${getSpacing(22)};
     padding-top: ${getSpacing(4)};
-    width: 800px;
+    width: ${(props: ContentProps) => (props.isLoginPage ? '100%' : '800px')};
   `,
 };
 
