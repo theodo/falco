87a655bb24226a367ebf40b066b14e9531f26ae6
Etienne Grall <etienne.grall@vestiairecollective.com>
2019-05-10 15:28:03 +0200

    Add second block to landing page

--- frontend/src/pages/Landing/Landing.style.tsx
+++ frontend/src/pages/Landing/Landing.style.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { colorUsage, fontFamily, fontSize, fontWeight, getSpacing, lineHeight } from 'stylesheet';
 
+interface ItemWithMarginProps {
+  margin?: string;
+}
+
 const Styles = {
   FirstBlockContainer: styled.div`
     width: 100%;
@@ -42,6 +46,49 @@ const Styles = {
     width: 475px;
     border-radius: 6px;
     box-shadow: 0 14px 21px 0 ${colorUsage.landingImageShadowBox};
+  `,
+
+  SecondBlockContainer: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: ${getSpacing(10)};
+  `,
+
+  SecondBlockColumn: styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    flex-grow: 1;
+    margin: ${(props: ItemWithMarginProps) => (props.margin ? props.margin : '0')};
+  `,
+
+  SubTitle: styled.div`
+    line-height: ${lineHeight.h3Text};
+    color: ${colorUsage.h3Text};
+    font-family: ${fontFamily.mainSans};
+    font-size: ${fontSize.h3Text};
+    font-weight: ${fontWeight.h3Text};
+    margin-bottom: ${getSpacing(2)};
+  `,
+
+  FunctionalIllustration: styled.img`
+    height: 191px;
+    width: 266px;
+    margin-bottom: ${getSpacing(6)};
+  `,
+
+  Description: styled.div`
+    width: 265px;
+    text-align: center;
+    line-height: ${lineHeight.bodyText};
+    color: ${colorUsage.bodyText};
+    font-family: ${fontFamily.mainSans};
+    font-size: ${fontSize.bodyText};
   `,
 };
 
--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -4,7 +4,11 @@ import { Redirect } from 'react-router';
 
 import LandingBlock from 'components/LandingBlock';
 import { routeDefinitions } from 'routes';
+import FalcoAnalysisImage from 'static/images/FalcoAnalysis.svg';
 import FalcoDashboardImage from 'static/images/FalcoDashboard.png';
+import FalcoMonitoringImage from 'static/images/FalcoMonitoring.svg';
+import FalcoOptimisationImage from 'static/images/FalcoOptimisation.svg';
+import { colorUsage, getSpacing } from 'stylesheet';
 import Style from './Landing.style';
 
 interface Props {
@@ -32,6 +36,37 @@ const Landing: React.FunctionComponent<Props> = props => {
             <Style.FalcoIllustration src={FalcoDashboardImage} />
           </Style.FirstBlockColumn>
         </Style.FirstBlockContainer>
+      </LandingBlock>
+      <LandingBlock backgroundColor={colorUsage.landingPageGlobalBackground}>
+        <Style.SecondBlockContainer>
+          <Style.SecondBlockColumn margin={`0 ${getSpacing(4)} 0 0`}>
+            <Style.SubTitle>
+              <FormattedMessage id="Landing.second_block.monitoring.title" />
+            </Style.SubTitle>
+            <Style.FunctionalIllustration src={FalcoMonitoringImage} />
+            <Style.Description>
+              <FormattedMessage id="Landing.second_block.monitoring.description" />
+            </Style.Description>
+          </Style.SecondBlockColumn>
+          <Style.SecondBlockColumn margin={`0 ${getSpacing(4)} 0 0`}>
+            <Style.SubTitle>
+              <FormattedMessage id="Landing.second_block.analysis.title" />
+            </Style.SubTitle>
+            <Style.FunctionalIllustration src={FalcoAnalysisImage} />
+            <Style.Description>
+              <FormattedMessage id="Landing.second_block.analysis.description" />
+            </Style.Description>
+          </Style.SecondBlockColumn>
+          <Style.SecondBlockColumn>
+            <Style.SubTitle>
+              <FormattedMessage id="Landing.second_block.optimisation.title" />
+            </Style.SubTitle>
+            <Style.FunctionalIllustration src={FalcoOptimisationImage} />
+            <Style.Description>
+              <FormattedMessage id="Landing.second_block.optimisation.description" />
+            </Style.Description>
+          </Style.SecondBlockColumn>
+        </Style.SecondBlockContainer>
       </LandingBlock>
     </React.Fragment>
   );
