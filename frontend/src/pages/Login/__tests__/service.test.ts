c42d1cdb7e2f6d6055fbe0e7f35e1825a75c7629
Vincent LARRAT (Theodo) <vincentla@theodo.fr>
2019-05-17 12:43:00 +0200

    [115] Adapt login tests

--- frontend/src/pages/Login/__tests__/service.test.ts
+++ frontend/src/pages/Login/__tests__/service.test.ts
@@ -2,17 +2,17 @@ import { handleSubmit, mapPropsToValues, validateForm } from '../service';
 
 describe('Login form service', () => {
   describe('validateForm function', () => {
-    it('Should return an error "Username required" in username field if username field is empty in values', () => {
+    it('Should return an error in username field if username field is empty in values', () => {
       const values = {
         username: '',
         password: 'azdaz',
       };
       const errors = validateForm(values);
 
-      expect(errors.username).toBe('Username required');
+      expect(errors.username).toBeDefined();
       expect(errors.password).not.toBeDefined();
     });
-    it('Should return an error "Password required" in password field if password field is empty in values', () => {
+    it('Should return an error in password field if password field is empty in values', () => {
       const values = {
         username: 'gandalf.leblanc@lacontee.co',
         password: '',
@@ -20,7 +20,7 @@ describe('Login form service', () => {
       const errors = validateForm(values);
 
       expect(errors.username).not.toBeDefined();
-      expect(errors.password).toBe('Password required');
+      expect(errors.password).toBeDefined();
     });
 
     it('Should return an empty object if the username is valid', () => {
