2f5466c6f29e21bee33b1bc7260a29799be03c2c
Etienne Grall <etienneg@theodo.fr>
2019-05-20 11:33:16 +0200

    Add tests for selector and reducer

--- frontend/src/redux/parameters/__tests__/reducer.test.ts
+++ frontend/src/redux/parameters/__tests__/reducer.test.ts
@@ -1,36 +1,37 @@
+import { MetricType } from 'redux/auditResults/types';
 import { updateDisplayedMetrics } from '../actions';
 import reducer, { ParametersState } from '../reducer';
 
 const initialState: ParametersState = {
-  displayedMetrics: [
-    'WPTMetricFirstViewTTI',
-    'WPTMetricFirstViewSpeedIndex',
-    'WPTMetricFirstViewLoadTime',
-  ],
+  metrics: {},
 };
 
 describe('Parameters reducer', () => {
   describe('UPDATE_DISPLAYED_METRICS case', () => {
+    const projectId = '12345';
+
     it('Should return an empty list when no metric is set to be displayed', () => {
-      const action = updateDisplayedMetrics({ displayedMetrics: [] });
-      const expectedState = { displayedMetrics: [] };
+      const action = updateDisplayedMetrics({ displayedMetrics: [], projectId });
+      const expectedState = { metrics: { [projectId]: [] } };
 
       expect(reducer(initialState, action)).toEqual(expectedState);
     });
 
     it('Should return the given list if the list is not empty', () => {
-      const newState: ParametersState = {
-        displayedMetrics: [
-          'WPTMetricFirstViewSpeedIndex',
-          'WPTMetricRepeatViewSpeedIndex',
-          'WPTMetricFirstViewFirstPaint',
-          'WPTMetricRepeatViewFirstPaint',
-        ],
-      };
-      const action = updateDisplayedMetrics(newState);
-      const expectedState = newState;
+      const displayedMetrics: MetricType[] = [
+        'WPTMetricFirstViewSpeedIndex',
+        'WPTMetricRepeatViewSpeedIndex',
+        'WPTMetricFirstViewFirstPaint',
+        'WPTMetricRepeatViewFirstPaint',
+      ];
+      const action = updateDisplayedMetrics({
+        displayedMetrics,
+        projectId,
+      });
 
-      expect(reducer(newState, action)).toEqual(expectedState);
+      const expectedState = { metrics: { [projectId]: displayedMetrics } };
+
+      expect(reducer(initialState, action)).toEqual(expectedState);
     });
   });
 });
