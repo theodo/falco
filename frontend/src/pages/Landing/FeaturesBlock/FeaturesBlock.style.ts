c9afa285d044d5d6671f56d7d8c7b2cae495ab34
Vincent Larrat <vincentla@theodo.fr>
2019-05-22 15:31:02 +0200

    [175] Enhance features block design on landing page

--- frontend/src/pages/Landing/FeaturesBlock/FeaturesBlock.style.ts
+++ frontend/src/pages/Landing/FeaturesBlock/FeaturesBlock.style.ts
@@ -1,31 +1,44 @@
 import styled from 'styled-components';
 import { colorUsage, fontFamily, fontSize, fontWeight, getSpacing, lineHeight } from 'stylesheet';
 
-interface ItemWithMarginProps {
-  margin?: string;
-}
-
 const Styles = {
   FeaturesBlockContainer: styled.div`
     width: 100%;
     height: 100%;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: ${getSpacing(10)};
+    padding: ${getSpacing(6)} ${getSpacing(20)};
+    font-family: ${fontFamily.mainSans};
   `,
 
-  FeaturesBlockColumn: styled.div`
+  FeaturesBlockTitle: styled.h2`
+    color: ${colorUsage.h2Text};
+    font-size: ${fontSize.h2Text};
+    line-height: ${lineHeight.h2Text};
+    font-weight: ${fontWeight.h2Text};
+  `,
+
+  FeaturesContainer: styled.div`
+    width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: ${getSpacing(6)} 0 ${getSpacing(4)} 0;
+  `,
+
+  FeatureContainer: styled.div`
+    height: 100%;
+    width: 266px;
     display: flex;
     align-items: center;
     flex-direction: column;
-    flex-grow: 1;
-    margin: ${(props: ItemWithMarginProps) => (props.margin ? props.margin : '0')};
   `,
 
-  FeaturesBlockTitle: styled.h4`
+  FeatureTitle: styled.h4`
     line-height: ${lineHeight.h4Text};
     color: ${colorUsage.h4Text};
     font-family: ${fontFamily.mainSans};
@@ -34,14 +47,13 @@ const Styles = {
     margin-bottom: ${getSpacing(2)};
   `,
 
-  FunctionalIllustration: styled.img`
-    height: 191px;
+  FeatureIllustration: styled.img`
     width: 266px;
-    margin-bottom: ${getSpacing(6)};
+    min-height: 210px;
+    margin-bottom: ${getSpacing(4)};
   `,
 
-  Description: styled.div`
-    width: 265px;
+  FeatureDescription: styled.div`
     text-align: center;
     line-height: ${lineHeight.bodyText};
     color: ${colorUsage.bodyText};
--- frontend/src/pages/Landing/FeaturesBlock/FeaturesBlock.tsx
+++ frontend/src/pages/Landing/FeaturesBlock/FeaturesBlock.tsx
@@ -15,33 +15,38 @@ const FeaturesBlock: React.FunctionComponent<Props> = ({ backgroundColor }) => {
   return (
     <LandingBlock backgroundColor={backgroundColor}>
       <Style.FeaturesBlockContainer>
-        <Style.FeaturesBlockColumn margin={`0 ${getSpacing(4)} 0 0`}>
-          <Style.FeaturesBlockTitle>
-            <FormattedMessage id="Landing.features_block.monitoring.title" />
-          </Style.FeaturesBlockTitle>
-          <Style.FunctionalIllustration src={FalcoMonitoringImage} />
-          <Style.Description>
-            <FormattedMessage id="Landing.features_block.monitoring.description" />
-          </Style.Description>
-        </Style.FeaturesBlockColumn>
-        <Style.FeaturesBlockColumn margin={`0 ${getSpacing(4)} 0 0`}>
-          <Style.FeaturesBlockTitle>
-            <FormattedMessage id="Landing.features_block.analysis.title" />
-          </Style.FeaturesBlockTitle>
-          <Style.FunctionalIllustration src={FalcoAnalysisImage} />
-          <Style.Description>
-            <FormattedMessage id="Landing.features_block.analysis.description" />
-          </Style.Description>
-        </Style.FeaturesBlockColumn>
-        <Style.FeaturesBlockColumn>
-          <Style.FeaturesBlockTitle>
-            <FormattedMessage id="Landing.features_block.optimisation.title" />
-          </Style.FeaturesBlockTitle>
-          <Style.FunctionalIllustration src={FalcoOptimisationImage} />
-          <Style.Description>
-            <FormattedMessage id="Landing.features_block.optimisation.description" />
-          </Style.Description>
-        </Style.FeaturesBlockColumn>
+        <Style.FeaturesBlockTitle>
+          <FormattedMessage id={'Landing.features_block.title'} />
+        </Style.FeaturesBlockTitle>
+        <Style.FeaturesContainer>
+          <Style.FeatureContainer>
+            <Style.FeatureTitle>
+              <FormattedMessage id="Landing.features_block.monitoring.title" />
+            </Style.FeatureTitle>
+            <Style.FeatureIllustration src={FalcoMonitoringImage} />
+            <Style.FeatureDescription>
+              <FormattedMessage id="Landing.features_block.monitoring.description" />
+            </Style.FeatureDescription>
+          </Style.FeatureContainer>
+          <Style.FeatureContainer>
+            <Style.FeatureTitle>
+              <FormattedMessage id="Landing.features_block.analysis.title" />
+            </Style.FeatureTitle>
+            <Style.FeatureIllustration src={FalcoAnalysisImage} />
+            <Style.FeatureDescription>
+              <FormattedMessage id="Landing.features_block.analysis.description" />
+            </Style.FeatureDescription>
+          </Style.FeatureContainer>
+          <Style.FeatureContainer>
+            <Style.FeatureTitle>
+              <FormattedMessage id="Landing.features_block.optimisation.title" />
+            </Style.FeatureTitle>
+            <Style.FeatureIllustration src={FalcoOptimisationImage} />
+            <Style.FeatureDescription>
+              <FormattedMessage id="Landing.features_block.optimisation.description" />
+            </Style.FeatureDescription>
+          </Style.FeatureContainer>
+        </Style.FeaturesContainer>
       </Style.FeaturesBlockContainer>
     </LandingBlock>
   );
