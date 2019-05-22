f319ae5f636396dbc8e4cec598890ad5b440ce49
Vincent Larrat <vincentla@theodo.fr>
2019-05-22 17:05:26 +0200

    [175] Enhance footer design on landing page

---
+++ frontend/src/pages/Landing/Footer/Footer.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { colorUsage, fontFamily, fontSize, getSpacing, lineHeight } from 'stylesheet';
+
+const Styles = {
+  FooterContainer: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${getSpacing(6)} ${getSpacing(20)} ${getSpacing(10)} ${getSpacing(20)};
+    line-height: ${lineHeight.introductionText};
+    color: ${colorUsage.introductionText};
+    font-family: ${fontFamily.mainSans};
+    font-size: ${fontSize.introductionText};
+  `,
+
+  TheodoLink: styled.a`
+    color: ${colorUsage.landingTheodoLink};
+    text-decoration: none;
+  `,
+};
+
+export default Styles;
--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { Redirect } from 'react-router';
 
-import LandingBlock from 'components/LandingBlock';
 import { routeDefinitions } from 'routes';
-import LogoTheodo from 'static/images/TheodoLogo.svg';
 import { colorUsage } from 'stylesheet';
 import CustomersBlock from './CustomersBlock';
 import FAQBlock from './FAQBlock';
 import FeaturesBlock from './FeaturesBlock';
+import Footer from './Footer';
 import IntroductionBlock from './IntroductionBlock';
-import Style from './Landing.style';
 
 interface Props {
   isUserAuthenticated: boolean;
@@ -26,13 +24,7 @@ export const Landing: React.FunctionComponent<Props> = props => {
       <CustomersBlock backgroundColor={colorUsage.landingPageGlobalBackground} />
       <FeaturesBlock />
       <FAQBlock backgroundColor={colorUsage.landingPageGlobalBackground} />
-      <LandingBlock>
-        <Style.FooterContainer>
-          <Style.TheodoLogoLink href={'https://www.theodo.fr'} target={'_blank'}>
-            <Style.TheodoLogo src={LogoTheodo} />
-          </Style.TheodoLogoLink>
-        </Style.FooterContainer>
-      </LandingBlock>
+      <Footer />
     </React.Fragment>
   );
 };
