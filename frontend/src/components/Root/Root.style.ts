a387369dbedab15e03a0912842974e31fa478766
Etienne Grall <etienneg@theodo.fr>
2019-06-05 10:53:00 +0200

    Refactor Root style

--- frontend/src/components/Root/Root.style.ts
+++ frontend/src/components/Root/Root.style.ts
@@ -9,40 +9,45 @@ interface PageBackgroundProps {
   isLandingPage: boolean;
 }
 
-const StyledRoot = {
-  PageContainer: styled.div`
-    height: auto;
-    width: auto;
-  `,
-  PageBackground: styled.div`
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    background-color: ${(props: PageBackgroundProps) => props.isLandingPage && colorUsage.landingPageGlobalBackground};
-  `,
-  Page: styled.div`
-    height: 100%;
-    font-family: ${fontFamily.mainSans};
-    width: 1440px;
+export const PageContainer = styled.div`
+  height: auto;
+  width: auto;
+`;
+PageContainer.displayName = 'PageContainer';
+
+export const PageBackground = styled.div<PageBackgroundProps>`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-color: ${props => props.isLandingPage && colorUsage.landingPageGlobalBackground};
+`;
+PageBackground.displayName = 'PageBackground';
+
+export const Page = styled.div`
+  height: 100%;
+  font-family: ${fontFamily.mainSans};
+  width: 1440px;
+  margin: auto;
+  background-color: ${colorUsage.defaultContentBackground};
+
+  @media only screen and (max-width: ${responsiveThreshold}) {
+    width: 320px;
     margin: auto;
-    background-color: ${colorUsage.defaultContentBackground};
+  }
+`;
+Page.displayName = 'Page';
 
-    @media only screen and (max-width: ${responsiveThreshold}) {
-      width: 320px;
-      margin: auto;
-    }
-  `,
-  Body: styled.div``,
-  Content: styled.div`
-    position: relative;
-    top: 100px;
-    left: ${(props: ContentProps) => (props.shouldDisplayMenu ? '490px' : '0')};
-    padding-left: ${(props: ContentProps) => (props.shouldDisplayMenu ? getSpacing(8) : '0')};
-    padding-right: ${(props: ContentProps) => (props.shouldDisplayMenu ? getSpacing(22) : '0')};
-    padding-top: ${getSpacing(4)};
-    width: ${(props: ContentProps) => (props.shouldDisplayMenu ? '800px' : '100%')};
-    background-color: ${colorUsage.defaultContentBackground};
-  `,
-};
+export const Body = styled.div``;
+Body.displayName = 'Body';
 
-export default StyledRoot;
+export const Content = styled.div<ContentProps>`
+  position: relative;
+  top: 100px;
+  left: ${props => (props.shouldDisplayMenu ? '490px' : '0')};
+  padding-left: ${props => (props.shouldDisplayMenu ? getSpacing(8) : '0')};
+  padding-right: ${props => (props.shouldDisplayMenu ? getSpacing(22) : '0')};
+  padding-top: ${getSpacing(4)};
+  width: ${props => (props.shouldDisplayMenu ? '800px' : '100%')};
+  background-color: ${colorUsage.defaultContentBackground};
+`;
+Content.displayName = 'Content';
