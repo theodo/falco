9d54a99505319f8ce6839014ba3e6646a593ae61
CecileSerene <cecilego@theodo.fr>
2019-06-13 12:07:31 +0200

    [280] Rearrange stylesheet

--- frontend/src/stylesheet.ts
+++ frontend/src/stylesheet.ts
@@ -41,6 +41,7 @@ const colorPalette = {
  * Ex: fill, border-color, background-color, color ...
  */
 export const colorUsage = {
+  // General
   h1Text: colorPalette.grey800,
   h2Text: colorPalette.grey800,
   h3Text: colorPalette.grey800,
@@ -48,32 +49,79 @@ export const colorUsage = {
   introductionText: colorPalette.grey800,
   bodyText: colorPalette.grey800,
   labelText: colorPalette.grey800,
-  link: colorPalette.primary500,
-  logoText: colorPalette.grey800,
-  menuLink: colorPalette.grey800,
-  auditLink: colorPalette.grey800,
-  pageBadgeText: colorPalette.accentBlue500,
-  pageBadgeBackground: colorPalette.accentBlue200,
-  pageBadgeSelectedText: colorPalette.white500,
-  pageBadgeSelectedBackground: colorPalette.white200,
-  scriptBadgeText: colorPalette.accentGreen500,
-  scriptBadgeBackground: colorPalette.accentGreen200,
-  scriptBadgeSelectedText: colorPalette.white500,
-  scriptBadgeSelectedBackground: colorPalette.white200,
   quoteText: colorPalette.grey800,
   smallText: colorPalette.grey800,
-  menuBackground: colorPalette.grey200,
+  logoText: colorPalette.grey800,
+  link: colorPalette.primary500,
+  loader: colorPalette.primary500,
+  defaultContentBackground: colorPalette.white500,
+  popinErrorText: colorPalette.red500,
+  popinErrorBackground: colorPalette.red200,
+  cancelButtonText: colorPalette.primary500,
+  cancelButtonBorder: colorPalette.primary500,
+  cancelButtonBackground: colorPalette.transparent,
+  validateButtonText: colorPalette.white500,
+  validateButtonBackground: colorPalette.primary500,
+  submitButtonText: colorPalette.white500,
+  submitButtonBackground: colorPalette.primary500,
+  checkboxBorder: colorPalette.grey500,
+  checkboxColor: colorPalette.primary500,
+  checkboxBackground: colorPalette.white500,
+  inputText: colorPalette.grey800,
+  inputTextPlaceholder: colorPalette.grey800,
+  inputTextFocused: colorPalette.primary500,
+  inputTextError: colorPalette.red500,
+  inputTextLabel: colorPalette.grey800,
+  inputTextBackground: colorPalette.transparent,
+  inputSelectText: colorPalette.grey800,
+  inputDisabledSelectText: colorPalette.grey500,
+  inputSelectBorder: colorPalette.grey500,
+
+  // Header
   headerLogo: colorPalette.primary500,
   headerFakeBackground: colorPalette.white500,
   headerShadowBox: colorPalette.grey350,
-  loader: colorPalette.primary500,
+  headerButtonHoverText: colorPalette.primary500,
+  accountMenuBackground: colorPalette.white500,
+  accountMenuShadow: colorPalette.grey400,
+  accountMenuText: colorPalette.grey800,
+  accountMenuUserInfosBlockBorder: colorPalette.grey300,
+  accountMenuActionItemHoverText: colorPalette.primary500,
+
+  // Menu
+  menuLink: colorPalette.grey800,
+  menuBackground: colorPalette.grey200,
+  menuArrow: colorPalette.grey800,
+  menuArrowSelected: colorPalette.white500,
+  menuLinkHover: colorPalette.primary500,
+
+  // Landing Page
+  landingPageGlobalBackground: colorPalette.grey100,
+  landingTheodoLink: colorPalette.primary500,
+  landingImageShadowBox: colorPalette.black100,
+  leadFormBorder: colorPalette.grey800,
+  leadSubmitButtonBackground: colorPalette.accentGreen500,
+  leadSubmitButtonWaitingBackground: colorPalette.grey800,
+  leadSubmitButtonLoaderPrimary: colorPalette.white500,
+  leadSubmitButtonLoaderSecondary: colorPalette.transparent,
+  leadSubmitButtonSuccessBackground: colorPalette.accentGreen500,
+  leadSubmitButtonSuccessCheck: colorPalette.white500,
+
+  // Login Page
+  connectButtonWaitingBackground: colorPalette.grey800,
+  connectButtonLoaderPrimary: colorPalette.white500,
+  connectButtonLoaderSecondary: colorPalette.transparent,
+  loginInputBorder: colorPalette.grey800,
+
+  // Audit
+  auditLink: colorPalette.grey800,
   auditLinkBorder: colorPalette.grey300,
   auditLinkHoverText: colorPalette.primary500,
   auditLinkSelectedText: colorPalette.white500,
   auditLinkSelectedBackground: colorPalette.primary500,
   auditLinkSelectedBorder: colorPalette.grey500,
-  menuArrow: colorPalette.grey800,
-  menuArrowSelected: colorPalette.white500,
+
+  // Graphs
   graphBorder: colorPalette.grey800,
   graphLine: colorPalette.primary500,
   graphTooltipCursor: colorPalette.lightGreen,
@@ -82,6 +130,18 @@ export const colorUsage = {
   graphTooltipActiveDot: colorPalette.lightGreen,
   graphTooltipActiveDotBorder: colorPalette.white500,
   graphText: colorPalette.grey800,
+  graphModalToggleButton: colorPalette.grey800,
+
+  // Metrics
+  metricsSettingsText: colorPalette.primary500,
+  metricInformationIcon: colorPalette.primary500,
+  metricTooltipBackground: colorPalette.grey300,
+  metricTooltipShadowBox: colorPalette.black200,
+  metricTooltipText: colorPalette.grey800,
+  metricsModalBackground: colorPalette.grey200,
+  metricsModalShadow: colorPalette.grey400,
+
+  // Projects
   projectsMenuBackground: colorPalette.white500,
   projectsMenuShadow: colorPalette.grey400,
   projectsMenuItemText: colorPalette.grey800,
@@ -90,62 +150,23 @@ export const colorUsage = {
   projectsMenuItemBorder: colorPalette.grey500,
   projectsMenuItemSnapshotBorder: colorPalette.grey800,
   projectsMenuItemStar: colorPalette.accentYellow500,
-  headerButtonHoverText: colorPalette.primary500,
+
+  // WebPageTest
   webPageTestLinkButtonBackground: colorPalette.primary500,
   webPageTestLinkButtonText: colorPalette.white500,
   webPageTestRadioButtonBorder: colorPalette.grey500,
   webPageTestRadioButtonColor: colorPalette.primary500,
   webPageTestRadioButtonBackground: colorPalette.white500,
-  inputSelectText: colorPalette.grey800,
-  inputDisabledSelectText: colorPalette.grey500,
-  inputSelectBorder: colorPalette.grey500,
-  accountMenuBackground: colorPalette.white500,
-  accountMenuShadow: colorPalette.grey400,
-  accountMenuText: colorPalette.grey800,
-  accountMenuUserInfosBlockBorder: colorPalette.grey300,
-  accountMenuActionItemHoverText: colorPalette.primary500,
-  popinErrorText: colorPalette.red500,
-  popinErrorBackground: colorPalette.red200,
-  metricsSettingsText: colorPalette.primary500,
-  menuLinkHover: colorPalette.primary500,
-  defaultContentBackground: colorPalette.white500,
-  landingPageGlobalBackground: colorPalette.grey100,
-  landingImageShadowBox: colorPalette.black100,
-  metricInformationIcon: colorPalette.primary500,
-  metricTooltipBackground: colorPalette.grey300,
-  metricTooltipShadowBox: colorPalette.black200,
-  metricTooltipText: colorPalette.grey800,
-  landingTheodoLink: colorPalette.primary500,
-  submitButtonText: colorPalette.white500,
-  submitButtonBackground: colorPalette.primary500,
-  leadFormBorder: colorPalette.grey800,
-  leadSubmitButtonBackground: colorPalette.accentGreen500,
-  leadSubmitButtonWaitingBackground: colorPalette.grey800,
-  leadSubmitButtonLoaderPrimary: colorPalette.white500,
-  leadSubmitButtonLoaderSecondary: colorPalette.transparent,
-  leadSubmitButtonSuccessBackground: colorPalette.accentGreen500,
-  leadSubmitButtonSuccessCheck: colorPalette.white500,
-  connectButtonWaitingBackground: colorPalette.grey800,
-  connectButtonLoaderPrimary: colorPalette.white500,
-  connectButtonLoaderSecondary: colorPalette.transparent,
-  inputText: colorPalette.grey800,
-  inputTextPlaceholder: colorPalette.grey800,
-  inputTextFocused: colorPalette.primary500,
-  inputTextError: colorPalette.red500,
-  inputTextLabel: colorPalette.grey800,
-  inputTextBackground: colorPalette.transparent,
-  loginInputBorder: colorPalette.grey800,
-  metricsModalBackground: colorPalette.grey200,
-  metricsModalShadow: colorPalette.grey400,
-  cancelButtonText: colorPalette.primary500,
-  cancelButtonBorder: colorPalette.primary500,
-  cancelButtonBackground: colorPalette.transparent,
-  validateButtonText: colorPalette.white500,
-  validateButtonBackground: colorPalette.primary500,
-  checkboxBorder: colorPalette.grey500,
-  checkboxColor: colorPalette.primary500,
-  checkboxBackground: colorPalette.white500,
-  graphModalToggleButton: colorPalette.grey800,
+
+  // Badge
+  pageBadgeText: colorPalette.accentBlue500,
+  pageBadgeBackground: colorPalette.accentBlue200,
+  pageBadgeSelectedText: colorPalette.white500,
+  pageBadgeSelectedBackground: colorPalette.white200,
+  scriptBadgeText: colorPalette.accentGreen500,
+  scriptBadgeBackground: colorPalette.accentGreen200,
+  scriptBadgeSelectedText: colorPalette.white500,
+  scriptBadgeSelectedBackground: colorPalette.white200,
 };
 
 export const fontFamily = {
@@ -154,6 +175,7 @@ export const fontFamily = {
 };
 
 export const fontSize = {
+  // General
   h1Text: '36px',
   h2Text: '30px',
   h3Text: '24px',
@@ -161,17 +183,10 @@ export const fontSize = {
   introductionText: '18px',
   labelText: '16px',
   bodyText: '14px',
-  link: '14px',
-  webPageTestLink: '14px',
-  logoText: '26px',
-  menuLink: '20px',
-  auditLink: '20px',
-  badgeText: '14px',
   quoteText: '24px',
   smallText: '10px',
-  graphText: '14px',
-  inputSelectText: '14px',
-  metricTooltip: '12px',
+  logoText: '26px',
+  link: '14px',
   submitButton: '14px',
   cancelButton: '14px',
   validateButton: '14px',
@@ -179,7 +194,28 @@ export const fontSize = {
   inputTextPlaceholder: '14px',
   inputTextLabel: '12px',
   inputErrorMessage: '12px',
+  inputSelectText: '14px',
+
+  // Menu
+  menuLink: '20px',
+
+  // Landing
   leadSubmitErrorMessage: '13px',
+
+  // Audit
+  auditLink: '20px',
+
+  // Graphs
+  graphText: '14px',
+
+  // Metrics
+  metricTooltip: '12px',
+
+  // WebPageTest
+  webPageTestLink: '14px',
+
+  // Badge
+  badgeText: '14px',
 };
 
 export const lineHeight = {
@@ -190,17 +226,10 @@ export const lineHeight = {
   introductionText: '26px',
   bodyText: '20px',
   labelText: '20px',
-  link: '20px',
-  logoText: '34px',
-  menuLink: '27px',
-  auditLink: '27px',
-  badgeText: '18px',
   quoteText: '34px',
   smallText: '15px',
-  graphText: '20px',
-  webPageTestLink: '18px',
-  inputSelectText: '18px',
-  metricTooltip: '16px',
+  link: '20px',
+  logoText: '34px',
   submitButton: '18px',
   cancelButton: '18px',
   validateButton: '18px',
@@ -208,6 +237,25 @@ export const lineHeight = {
   inputTextPlaceholder: '16px',
   inputTextLabel: '16px',
   inputErrorMessage: '14px',
+  inputSelectText: '18px',
+
+  // Menu
+  menuLink: '27px',
+
+  // Audits
+  auditLink: '27px',
+
+  // Graphs
+  graphText: '14px',
+
+  // Metrics
+  metricTooltip: '16px',
+
+  // WebPageTest
+  webPageTestLink: '18px',
+
+  // Badge
+  badgeText: '18px',
 };
 
 export const fontWeight = {
@@ -218,20 +266,36 @@ export const fontWeight = {
   labelText: '500',
   bodyText: '500',
   logoText: 'bold',
-  menuLink: 'bold',
-  auditLink: 'bold',
-  badgeText: 'bold',
-  graphText: 'bold',
   linkText: 'bold',
-  webPageTestLink: 'bold',
-  accountMenuActionItemHoverText: 'bold',
-  metricTooltip: '500',
-  landingTheodoLink: 'bold',
   submitButton: 'bold',
   cancelButton: 'bold',
   validateButton: 'bold',
   inputErrorMessage: 'bold' as 'bold',
   inputTextLabel: 'bold',
+
+  // Header
+  accountMenuActionItemHoverText: 'bold',
+
+  // Menu
+  menuLink: 'bold',
+
+  // Landing
+  landingTheodoLink: 'bold',
+
+  // Audits
+  auditLink: 'bold',
+
+  // Graphs
+  graphText: 'bold',
+
+  // Metrics
+  metricTooltip: '500',
+
+  // WebPageTest
+  webPageTestLink: 'bold',
+
+  // Badge
+  badgeText: 'bold',
 };
 
 export const fontStyle = {
