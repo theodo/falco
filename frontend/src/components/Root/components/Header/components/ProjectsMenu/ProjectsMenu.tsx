303698a392d2df06d2758078454f137633a85bbc
fargito <fr.farge96@gmail.com>
2019-06-14 17:58:38 +0200

    remove star from current project item in menu

--- frontend/src/components/Root/components/Header/components/ProjectsMenu/ProjectsMenu.tsx
+++ frontend/src/components/Root/components/Header/components/ProjectsMenu/ProjectsMenu.tsx
@@ -1,14 +1,27 @@
 import dayjs from 'dayjs';
-import { Star } from 'icons';
 import React from 'react';
 import { FormattedMessage, InjectedIntlProps } from 'react-intl';
 import { ProjectType } from 'redux/projects/types';
 import { routeDefinitions } from 'routes';
-import { colorUsage, getSpacing } from 'stylesheet';
+import { getSpacing } from 'stylesheet';
 
 import ErrorMessage from 'components/ErrorMessage';
 import Loader from 'components/Loader';
-import Style from './ProjectsMenu.style';
+import {
+  Container,
+  CurrentProjectItem,
+  CurrentProjectItemSnapshot,
+  CurrentProjectItemTitle,
+  CurrentProjectItemTitleBlock,
+  CurrentProjectItemTitleContainer,
+  ProjectItem,
+  ProjectItemContainer,
+  ProjectItemLastAudit,
+  ProjectItemSnapshot,
+  ProjectItemSnapshotContainer,
+  ProjectItemTitle,
+  ProjectItemTitleBlock,
+} from './ProjectsMenu.style';
 
 interface OwnProps {
   currentProject: ProjectType | undefined;
@@ -43,81 +56,78 @@ export const ProjectsMenu: React.FunctionComponent<Props> = ({
 
   const renderCurrentProjectItem = () =>
     currentProject && (
-      <Style.CurrentProjectItem>
-        <Style.ProjectItemSnapshotContainer>
-          <Style.CurrentProjectItemSnapshot
+      <CurrentProjectItem>
+        <ProjectItemSnapshotContainer>
+          <CurrentProjectItemSnapshot
             src={currentProject.screenshotUrl}
             margin={`0 ${getSpacing(6)} 0 0`}
           />
-        </Style.ProjectItemSnapshotContainer>
-        <Style.CurrentProjectItemTitleBlock>
-          <Style.CurrentProjectItemTitleContainer margin={`0 0 ${getSpacing(1)} 0`}>
-            <Style.CurrentProjectItemStarContainer margin={`0 ${getSpacing(2)} 0 0`}>
-              <Star color={colorUsage.projectsMenuItemStar} />
-            </Style.CurrentProjectItemStarContainer>
-            <Style.CurrentProjectItemTitle>{currentProject.name}</Style.CurrentProjectItemTitle>
-          </Style.CurrentProjectItemTitleContainer>
-          <Style.ProjectItemLastAudit>
+        </ProjectItemSnapshotContainer>
+        <CurrentProjectItemTitleBlock>
+          <CurrentProjectItemTitleContainer margin={`0 0 ${getSpacing(1)} 0`}>
+            <CurrentProjectItemTitle>{currentProject.name}</CurrentProjectItemTitle>
+          </CurrentProjectItemTitleContainer>
+          <ProjectItemLastAudit>
             {intl.formatMessage({ id: `Header.last_audit_intro` })}{' '}
             {dayjs(currentProject.latestAuditAt)
               .locale(intl.locale)
               .fromNow()}
-          </Style.ProjectItemLastAudit>
-        </Style.CurrentProjectItemTitleBlock>
-      </Style.CurrentProjectItem>
+          </ProjectItemLastAudit>
+        </CurrentProjectItemTitleBlock>
+      </CurrentProjectItem>
     );
 
   const renderProjectItem = (project: ProjectType | null) =>
     project && (
-      <Style.ProjectItem
+      <ProjectItem
         key={project.uuid}
         to={routeDefinitions.projectDetails.path.replace(':projectId', project.uuid)}
       >
-        <Style.ProjectItemSnapshotContainer>
-          <Style.ProjectItemSnapshot
+        <ProjectItemSnapshotContainer>
+          <ProjectItemSnapshot
             src={project.screenshotUrl}
             margin={`0 ${getSpacing(4)} 0 0`}
           />
-        </Style.ProjectItemSnapshotContainer>
-        <Style.ProjectItemTitleBlock>
-          <Style.ProjectItemTitle>{project.name}</Style.ProjectItemTitle>
-          <Style.ProjectItemLastAudit>
+        </ProjectItemSnapshotContainer>
+        <ProjectItemTitleBlock>
+          <ProjectItemTitle>{project.name}</ProjectItemTitle>
+          <ProjectItemLastAudit>
             {intl.formatMessage({ id: `Header.last_audit_intro` })}{' '}
             {dayjs(project.latestAuditAt)
               .locale(intl.locale)
               .fromNow()}
-          </Style.ProjectItemLastAudit>
-        </Style.ProjectItemTitleBlock>
-      </Style.ProjectItem>
+          </ProjectItemLastAudit>
+        </ProjectItemTitleBlock>
+      </ProjectItem>
     );
 
   if (isVisible) {
     if (null === projects) {
       return (
-        <Style.Container position={position} right={right}>
+        <Container position={position} right={right}>
           <Loader minHeight={'200px'} />
-        </Style.Container>
+        </Container>
       );
     }
 
     if (0 === projects.length) {
       return (
-        <Style.Container position={position} right={right}>
+        <Container position={position} right={right}>
           <ErrorMessage>
             <FormattedMessage id="Projects.no_project_error" />
           </ErrorMessage>
-        </Style.Container>
+        </Container>
       );
     }
     return (
-      <Style.Container position={position} right={right}>
+      <Container position={position} right={right}>
         {renderCurrentProjectItem()}
-        <Style.ProjectItemContainer>
+        <ProjectItemContainer>
           {projects
             .filter((project: ProjectType | null) => currentProject !== project)
             .map((project: ProjectType | null) => renderProjectItem(project))}
-        </Style.ProjectItemContainer>
-      </Style.Container>
+        </ProjectItemContainer>
+      </Container>
     );
   }
   return <div />;
