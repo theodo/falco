87b35da9473fc3127293415e64f6121cef6e885d
CecileSerene <cecilego@theodo.fr>
2019-06-19 16:40:48 +0200

    [321] Refactor style

--- frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseBlock.style.tsx
+++ frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseBlock.style.tsx
@@ -5,33 +5,32 @@ interface ItemWithMarginProps {
   margin?: string;
 }
 
-const Style = {
-  Container: styled.div`
+export const Container = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-  `,
+`;
 
-  SubTitle: styled.div`
+export const SubTitle = styled.div`
     margin: ${(props: ItemWithMarginProps) => (props.margin ? props.margin : '0')};
     line-height: ${lineHeight.h3Text};
     color: ${colorUsage.h3Text};
     font-family: ${fontFamily.mainSans};
     font-size: ${fontSize.h3Text};
     font-weight: ${fontWeight.h3Text};
-  `,
+`;
 
-  LighthouseNote: styled.div`
+export const LighthouseNote = styled.div`
     margin: ${(props: ItemWithMarginProps) => (props.margin ? props.margin : '0')};
     color: ${colorUsage.bodyText};
     font-family: ${fontFamily.mainSans};
     font-size: ${fontSize.bodyText};
     font-style: ${fontStyle.lighthouseNote};
     line-height: ${lineHeight.bodyText};
-  `,
+`;
 
-  LighthouseFrame: styled.iframe`
+export const LighthouseFrame = styled.iframe`
     width: 100%;
     height: 570px;
   `,
--- frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseBlock.tsx
+++ frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseBlock.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { getSpacing } from 'stylesheet';
-import Style from './LighthouseBlock.style';
+import { Container, LighthouseFrame, LighthouseNote, SubTitle } from './LighthouseBlock.style';
 
 export interface OwnProps {
   lighthouseUrl: string;
@@ -9,15 +9,15 @@ export interface OwnProps {
 
 const LighthouseBlock: React.FunctionComponent<OwnProps> = ({ lighthouseUrl }) => {
   return (
-    <Style.Container>
-      <Style.SubTitle margin={`0 0 ${getSpacing(5)} 0}`}>
+    <Container>
+      <SubTitle margin={`0 0 ${getSpacing(5)} 0}`}>
         <FormattedMessage id="Audits.lighthouse_report" />
-      </Style.SubTitle>
-      <Style.LighthouseNote margin={`0 0 ${getSpacing(4)} 0}`}>
+      </SubTitle>
+      <LighthouseNote margin={`0 0 ${getSpacing(4)} 0}`}>
         <FormattedMessage id="Audits.lighthouse_note" />
-      </Style.LighthouseNote>
-      <Style.LighthouseFrame src={lighthouseUrl} />
-    </Style.Container>
+      </LighthouseNote>
+      <LighthouseFrame src={lighthouseUrl}/>
+    </Container>
   );
 };
 
