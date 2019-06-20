3e7bf5ab321985742fdfd9e0b2ca60f27ef7d3cd
fargito <fr.farge96@gmail.com>
2019-06-20 15:36:25 +0200

    add selector for lastClickOnWhatsNew

--- frontend/src/redux/content/selectors.ts
+++ frontend/src/redux/content/selectors.ts
@@ -1,3 +1,5 @@
 import { RootState } from 'redux/types';
 
 export const getLastUpdateOfWhatsNew = (store: RootState) => store.content.lastUpdateOfWhatsNew;
+
+export const getLastClickOnWhatsNew = (store: RootState) => store.content.lastClickOnWhatsNew;
