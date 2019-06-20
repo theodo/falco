91e49959bf25ed4e0422dcb349e9d22b819f0faa
fargito <fr.farge96@gmail.com>
2019-06-20 15:09:41 +0200

    add persist reducer for lastClickOnWhatsNew

--- frontend/src/redux/content/reducer.ts
+++ frontend/src/redux/content/reducer.ts
@@ -1,15 +1,30 @@
 import { AnyAction } from 'redux';
 import { ActionType, getType } from 'typesafe-actions';
 
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {
   fetchLastUpdateOfWhatsNew,
 } from './actions';
 
 export type ContentAction = ActionType<typeof fetchLastUpdateOfWhatsNew>;
 
-export type ContentState = Readonly<{ lastUpdateOfWhatsNew: string | null }>;
+export type ContentState = Readonly<{
+  lastUpdateOfWhatsNew: string | null;
+  lastClickOnWhatsNew: string | null;
+}>;
 
-const initialState: ContentState = { lastUpdateOfWhatsNew: null };
+const initialState: ContentState = {
+  lastUpdateOfWhatsNew: null,
+  lastClickOnWhatsNew: null,
+};
+
+const persistConfig = {
+  key: 'content',
+  whitelist: ['lastClickOnWhatsNew'],
+  blacklist: ['lastUpdateOfWhatsNew'],
+  storage,
+};
 
 const reducer = (state: ContentState = initialState, action: AnyAction) => {
   const typedAction = action as ContentAction;
@@ -24,4 +39,4 @@ const reducer = (state: ContentState = initialState, action: AnyAction) => {
   }
 };
 
-export default reducer;
+export default persistReducer(persistConfig, reducer);
