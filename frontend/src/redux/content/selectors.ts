cec02ce43036fce98db58ae77f1ccf60c84693b5
fargito <fr.farge96@gmail.com>
2019-06-21 10:52:56 +0200

    add doc for content selectors

--- frontend/src/redux/content/selectors.ts
+++ frontend/src/redux/content/selectors.ts
@@ -6,10 +6,17 @@ export const getLastUpdateOfWhatsNew = (store: RootState) => store.content.lastU
 export const getLastClickOnWhatsNew = (store: RootState) => store.content.lastClickOnWhatsNew;
 
 export const getShouldDisplayWhatsNewNotification = (store: RootState) => {
+    /**
+     * this function allows to select whether the user has cliked on the
+     * "what's new" link since the last update (got from the server)
+     * 3 use cases :
+     * - The user has never clicked on the link => display notification
+     * - No update date has been received from the server => don't display notification
+     * - Display notification if the date of the last click is before the date of the last update
+     */
     const lastClickOnWhatsNew = getLastClickOnWhatsNew(store);
     const lastUpdateOfWhatsNew = getLastUpdateOfWhatsNew(store);
     if (!lastClickOnWhatsNew) {
-        // display notification to the user if he has never clicked on the link
         return true;
     }
     if (!lastUpdateOfWhatsNew) {
