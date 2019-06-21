004377aadd61346d8874adf1bab6443b27b7ca60
fargito <fr.farge96@gmail.com>
2019-06-21 17:44:18 +0200

    add intl

--- frontend/src/components/Root/components/NotFoundComponent/NotFoundComponent.tsx
+++ frontend/src/components/Root/components/NotFoundComponent/NotFoundComponent.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { NotFoundButton, NotFoundContainer, NotFoundInsideContainer, NotFoundSubTitle, NotFoundTitle } from './NotFoundComponent.style';
 
 const NotFoundComponent: React.FunctionComponent<any> = () => (
     <NotFoundContainer>
         <NotFoundInsideContainer>
-            <NotFoundTitle>404 Not Found</NotFoundTitle>
-            <NotFoundSubTitle>Sorry, this page does not exist</NotFoundSubTitle>
-            <NotFoundButton href="/">Back to homepage</NotFoundButton>
+            <NotFoundTitle>
+                <FormattedMessage id="notFound.404Title" />
+            </NotFoundTitle>
+            <NotFoundSubTitle>
+                <FormattedMessage id="notFound.404Subtitle" />
+            </NotFoundSubTitle>
+            <NotFoundButton href="/">
+                <FormattedMessage id="notFound.404Button" />
+            </NotFoundButton>
         </NotFoundInsideContainer>
     </NotFoundContainer>
 );
 
-export default NotFoundComponent;
+export default injectIntl(NotFoundComponent);
