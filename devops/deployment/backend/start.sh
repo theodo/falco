6c2752d0838b20bfcafe1a8b30510ca3bea18dca
fargito <fr.farge96@gmail.com>
2019-06-26 16:26:10 +0200

    hide gunicorn config in server headers

--- devops/deployment/backend/start.sh
+++ devops/deployment/backend/start.sh
@@ -6,4 +6,4 @@ python ./manage.py migrate --noinput
 # Create cache table
 python ./manage.py createcachetable
 
-gunicorn --bind 0.0.0.0:80 --chdir /code --workers 2 --access-logfile /var/log/falco/gunicorn-access.log --error-logfile /var/log/falco/gunicorn-error.log root.wsgi:application
+gunicorn --bind 0.0.0.0:80 --chdir /code --workers 2 --access-logfile /var/log/falco/gunicorn-access.log --error-logfile /var/log/falco/gunicorn-error.log -c root/gunicorn.py root.wsgi:application
