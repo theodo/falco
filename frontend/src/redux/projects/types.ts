5ab23d94092458881ebcff87cb95ed520450f4e7
fargito <fr.farge96@gmail.com>
2019-06-27 17:46:35 +0200

    add latestAuditStatusHistory to redux store

--- frontend/src/redux/projects/types.ts
+++ frontend/src/redux/projects/types.ts
@@ -10,11 +10,26 @@ export interface PageType {
   name: string;
   url: string;
   uuid: string;
+  latestAuditStatusHistory: AuditStatusHistoryType;
+}
+
+export interface ApiPageType {
+  name: string;
+  url: string;
+  uuid: string;
+  latest_audit_status_history: ApiAuditStatusHistoryType;
 }
 
 export interface ScriptType {
   uuid: string;
   name: string;
+  latestAuditStatusHistory: AuditStatusHistoryType;
+}
+
+export interface ApiScriptType {
+  uuid: string;
+  name: string;
+  latest_audit_status_history: ApiAuditStatusHistoryType;
 }
 
 export interface ProjectType {
@@ -35,11 +50,23 @@ export interface AuditParametersAPIType {
   network_shape: string;
 }
 
+export interface AuditStatusHistoryType {
+  createdAt: string;
+  status: "SUCCESS" | "REQUESTED" | "PENDING" | "ERROR";
+  details: string;
+}
+
+export interface ApiAuditStatusHistoryType {
+  created_at: string;
+  status: "SUCCESS" | "REQUESTED" | "PENDING" | "ERROR";
+  details: string;
+}
+
 export interface ApiProjectType {
   uuid: string;
   name: string;
-  pages: PageType[];
-  scripts: ScriptType[];
+  pages: ApiPageType[];
+  scripts: ApiScriptType[];
   audit_parameters_list: AuditParametersAPIType[];
   screenshot_url: string;
   latest_audit_at: string;
