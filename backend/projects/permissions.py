7edbcc9e3de40caf6d1fa88b4536ebcd571de4eb
fargito <fr.farge96@gmail.com>
2019-06-28 15:07:58 +0200

    filter active projects in project_list and prevent access if project is not active

--- backend/projects/permissions.py
+++ backend/projects/permissions.py
@@ -16,6 +16,9 @@ def check_if_member_of_project(member_id, project_uuid):
             detail="You are not authorized to access to this project"
         )
 
+    if not project.is_active:
+        raise PermissionDenied(detail="This project has been archived")
+
 
 def check_if_admin_of_project(member_id, project_uuid):
     project = Project.objects.get(uuid=project_uuid)
@@ -23,3 +26,6 @@ def check_if_admin_of_project(member_id, project_uuid):
 
     if (not project) or (not user) or (user not in project.admins.all()):
         raise PermissionDenied(detail="You do not have admin access to this project")
+
+    if not project.is_active:
+        raise PermissionDenied(detail="This project has been archived")
