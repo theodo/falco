7acb5abe501a853fc26ce827d9eb08710da077ef
fargito <fr.farge96@gmail.com>
2019-07-02 18:43:19 +0200

    apply page selector in audit.wrap

--- frontend/src/pages/Audits/Audits.wrap.tsx
+++ frontend/src/pages/Audits/Audits.wrap.tsx
@@ -18,17 +18,13 @@ import { RootState } from 'redux/types';
 import { injectIntl } from 'react-intl';
 import { Dispatch } from 'redux';
 import { fetchAuditResultsRequest } from 'redux/auditResults';
-import { modelizePagesToById } from 'redux/entities/pages/modelizer';
+import { getPage } from 'redux/entities/pages/selectors';
 import { Audits, OwnProps } from './Audits';
 
 const mapStateToProps = (state: RootState, props: OwnProps) => ({
   project: state.entities.projects.byId ? state.entities.projects.byId[props.match.params.projectId] : undefined,
   page:
-    state.entities.projects.byId && state.entities.projects.byId[props.match.params.projectId]
-      ? modelizePagesToById(state.entities.projects.byId[props.match.params.projectId].pages)[
-      props.match.params.pageOrScriptId
-      ]
-      : undefined,
+    getPage(state, props.match.params.pageOrScriptId) || undefined,
   script:
     state.entities.projects.byId && state.entities.projects.byId[props.match.params.projectId]
       ? modelizeScripts(state.entities.projects.byId[props.match.params.projectId].scripts)[
