ea39cd2ff42cba485bb472622c15827035164c03
fargito <fr.farge96@gmail.com>
2019-07-02 16:30:32 +0200

    create actions for pages

---
+++ frontend/src/redux/entities/pages/actions.ts
@@ -0,0 +1,12 @@
+import { createAsyncAction } from "typesafe-actions";
+import { PageType } from "./types";
+
+export const fetchPageAction = createAsyncAction(
+    'Entities/Pages/FETCH_PAGE_REQUEST',
+    'Entities/Pages/FETCH_PAGE_SUCCESS',
+    'Entities/Pages/FETCH_PAGE_FAILURE',
+)<{}, { byId: Record<string, PageType>; }, { errorMessage: string }>();
+
+export default {
+    fetchPageAction,
+}
