288b2501df2e3007fad06c5872495ef81f0ff31a
fargito <fr.farge96@gmail.com>
2019-07-03 11:05:00 +0200

    create modelizeScript

--- frontend/src/redux/entities/projects/modelizer.ts
+++ frontend/src/redux/entities/projects/modelizer.ts
@@ -1,4 +1,4 @@
-import { modelizeAuditStatusHistory } from 'redux/auditResults/modelizer';
+import { modelizeScript } from '../scripts/modelizer';
 import { ApiProjectType, ProjectType } from './types';
 
 export const modelizeProject = (project: ApiProjectType): Record<string, ProjectType> => ({
@@ -6,11 +6,7 @@ export const modelizeProject = (project: ApiProjectType): Record<string, Project
     uuid: project.uuid,
     name: project.name,
     pagesIds: project.pages.map(page => page.uuid),
-    scripts: project.scripts.map(script => ({
-      uuid: script.uuid,
-      name: script.name,
-      latestAuditStatusHistories: script.latest_audit_status_histories.map(modelizeAuditStatusHistory),
-    })),
+    scripts: project.scripts.map(modelizeScript),
     auditParametersList: project.audit_parameters_list.map(auditParameters => ({
       uuid: auditParameters.uuid,
       name: auditParameters.name,
--- frontend/src/redux/entities/scripts/modelizer.ts
+++ frontend/src/redux/entities/scripts/modelizer.ts
@@ -1,4 +1,12 @@
-import { ScriptType } from "./types";
+import { modelizeAuditStatusHistory } from "redux/auditResults/modelizer";
+import { ApiScriptType, ScriptType } from "./types";
+
+
+export const modelizeScript = (apiScript: ApiScriptType): ScriptType => ({
+    uuid: apiScript.uuid,
+    name: apiScript.name,
+    latestAuditStatusHistories: apiScript.latest_audit_status_histories.map(modelizeAuditStatusHistory),
+})
 
 export const modelizeScriptsToById = (scripts: ScriptType[]): Record<string, ScriptType> => {
     return scripts.reduce((scriptsById, script) => {
