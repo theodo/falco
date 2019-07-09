fe603e01e8fa91ecb4615a3ba922f9983d4c87a0
fargito <fr.farge96@gmail.com>
2019-07-09 11:39:24 +0200

    factorize projects modelization and save sagas

--- frontend/src/redux/entities/projects/sagas.ts
+++ frontend/src/redux/entities/projects/sagas.ts
@@ -2,8 +2,6 @@ import { all, call, put, takeEvery } from 'redux-saga/effects';
 import { makeGetRequest } from 'services/networking/request';
 import { ActionType, getType } from 'typesafe-actions';
 
-import { handleAPIExceptions } from 'services/networking/handleAPIExceptions';
-
 import { fetchAuditParametersAction } from '../auditParameters/actions';
 import { modelizeApiAuditParametersListToById } from '../auditParameters/modelizer';
 import { ApiAuditParametersType } from '../auditParameters/types';
@@ -25,25 +23,41 @@ import {
   fetchProjectsRequest,
   fetchProjectSuccess,
 } from './actions';
-import { modelizeProject, modelizeProjects } from './modelizer';
+import { modelizeProjects } from './modelizer';
 import { ApiProjectType } from './types';
 
-function* fetchProjectsFailedHandler(error: Error) {
-  yield put(fetchProjectError({ projectId: null, errorMessage: error.message }));
-}
 
-function* fetchProjectFailedHandler(error: Error, actionPayload: Record<string, any>) {
-  yield put(fetchProjectError({ projectId: actionPayload.projectId, errorMessage: error.message }));
-}
+function* fetchProjects() {
+  try {
+    const endpoint = '/api/projects/';
+    const { body: projects }: { body: ApiProjectType[] } = yield call(
+      makeGetRequest,
+      endpoint,
+      true,
+      null,
+    );
+    yield saveProjectsToStore(projects);
+  } catch (error) {
+    yield put(fetchProjectError({ projectId: action.payload.currentProjectId || null, errorMessage: error.toString() }));
+  }
+};
 
-export function* fetchProjects() {
-  const endpoint = '/api/projects/';
-  const { body: projects }: { body: ApiProjectType[] } = yield call(
-    makeGetRequest,
-    endpoint,
-    true,
-    null,
-  );
+function* fetchProject(action: ActionType<typeof fetchProjectRequest>) {
+  try {
+    const endpoint = `/api/projects/${action.payload.projectId}/`;
+    const { body: project }: { body: ApiProjectType } = yield call(
+      makeGetRequest,
+      endpoint,
+      true,
+      null,
+    );
+    yield saveProjectsToStore([project]);
+  } catch (error) {
+    yield put(fetchProjectError({ projectId: action.payload.projectId, errorMessage: error.toString() }));
+  }
+};
+
+function* saveProjectsToStore(projects: ApiProjectType[]) {
   yield put(fetchPageAction.success({
     byId: modelizeApiPagesToById(projects.reduce((apiPages: ApiPageType[], project: ApiProjectType) => {
       return apiPages.concat(project.pages);
@@ -83,24 +97,14 @@ export function* fetchProjects() {
   yield put(fetchProjectSuccess({ byId: modelizeProjects(projects) }));
 };
 
-export function* fetchProject(action: ActionType<typeof fetchProjectRequest>) {
-  const endpoint = `/api/projects/${action.payload.projectId}/`;
-  const { body: project }: { body: ApiProjectType } = yield call(
-    makeGetRequest,
-    endpoint,
-    true,
-    null,
-  );
-  yield put(fetchProjectSuccess({ byId: modelizeProject(project) }));
-}
 
 export default function* projectsSaga() {
   yield takeEvery(
     getType(fetchProjectRequest),
-    handleAPIExceptions(fetchProject, fetchProjectFailedHandler),
+    fetchProject,
   );
   yield takeEvery(
     getType(fetchProjectsRequest),
-    handleAPIExceptions(fetchProjects, fetchProjectsFailedHandler),
+    fetchProjects,
   );
 };
