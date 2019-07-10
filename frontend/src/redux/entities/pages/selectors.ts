d46f862008ecb95ad8b8d2715bc322ae7f662a2d
fargito <fr.farge96@gmail.com>
2019-07-10 18:09:23 +0200

    add running audit selector

--- frontend/src/redux/entities/pages/selectors.ts
+++ frontend/src/redux/entities/pages/selectors.ts
@@ -5,18 +5,18 @@ import { PageType } from "./types";
 
 
 export const getPage = (state: RootState, pageId: string): PageType | null | undefined => {
-    return state.entities.pages.byId ? state.entities.pages.byId[pageId] : undefined;
+  return state.entities.pages.byId ? state.entities.pages.byId[pageId] : undefined;
 };
 
 export const getPageLatestAuditStatusHistory = (state: RootState, pageId: string): AuditStatusHistoryType | undefined | null => {
-    const auditParametersId = getCurrentAuditParametersId(state);
-    if (!auditParametersId) {
-        return null;
-    };
-    return state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId
-        ? state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId[pageId]
-            ? state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId[pageId][auditParametersId]
-            : null
-            || null
-        : null;
+  const auditParametersId = getCurrentAuditParametersId(state);
+  if (!auditParametersId) {
+    return null;
+  };
+  return state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId
+    ? state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId[pageId]
+      ? state.entities.auditStatusHistories.byPageOrScriptIdAndAuditParametersId[pageId][auditParametersId]
+      : null
+      || null
+    : null;
 };
