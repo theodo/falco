39701dddd1d0e67ee492c5d2f55a73a8987a8c60
fargito <fr.farge96@gmail.com>
2019-07-10 16:31:15 +0200

    move audits polling sagas to audits folder

--- frontend/src/redux/entities/audits/sagas.ts
+++ frontend/src/redux/entities/audits/sagas.ts
@@ -1,8 +1,13 @@
-import { all, call, put, takeEvery } from 'redux-saga/effects'
+import { delay } from 'redux-saga';
+import { all, call, fork, put, race, take, takeEvery } from 'redux-saga/effects'
+import { fetchAuditResultsRequest } from 'redux/auditResults';
 import { handleAPIExceptions } from 'services/networking/handleAPIExceptions';
-import { makePostRequest } from 'services/networking/request';
+import { makeGetRequest, makePostRequest } from 'services/networking/request';
 import { ActionType, getType } from "typesafe-actions";
-import { launchAuditAction, pollAuditStatusAction } from "./actions";
+import { fetchAuditStatusHistoriesAction } from '../auditStatusHistories';
+import { modelizeApiAuditStatusHistoriesToByPageOrScriptIdAndAuditParametersId } from '../auditStatusHistories/modelizer';
+import { ApiAuditStatusHistoryType } from '../auditStatusHistories/types';
+import { launchAuditAction, pollAuditStatusAction, stopPollingAuditStatusAction } from "./actions";
 import { modelizeAudit } from './modelizer';
 import { ApiAuditType } from "./types";
 
@@ -29,9 +34,61 @@ function* launchAuditsSaga(action: ActionType<typeof launchAuditAction.request>)
     }));
 };
 
+function* pollAuditStatusHistories(auditId: string) {
+    try {
+        const endpoint = `/api/audits/${auditId}/status`;
+        const { body: auditStatusHistory }: { body: ApiAuditStatusHistoryType } = yield call(makeGetRequest, endpoint, true, null);
+
+        // save the response to the store before continuing the polling
+        yield put(fetchAuditStatusHistoriesAction.success({
+            byPageOrScriptIdAndAuditParametersId: modelizeApiAuditStatusHistoriesToByPageOrScriptIdAndAuditParametersId([auditStatusHistory]),
+        }));
+
+        const pollingIsFinished = (auditStatusHistory.status === "SUCCESS" || auditStatusHistory.status === "ERROR");
+        if (pollingIsFinished) {
+            yield put(stopPollingAuditStatusAction({
+                lastAuditStatusHistory: auditStatusHistory,
+            }));
+        }
+        else {
+            // wait for 10 seconds before the next polling
+            yield call(delay, 10000);
+            yield put(pollAuditStatusAction({ auditId }))
+        };
+    } catch (error) {
+        yield put(fetchAuditStatusHistoriesAction.failure({ errorMessage: error.toString() }));
+    };
+};
+
+function* fetchAuditResultsAfterPolling(action: ActionType<typeof stopPollingAuditStatusAction>) {
+    const auditStatusHistory = action.payload.lastAuditStatusHistory;
+    yield put(fetchAuditResultsRequest({
+        auditParametersId: auditStatusHistory.parameters_id,
+        pageOrScriptId: auditStatusHistory.page_id || auditStatusHistory.script_id,
+        type: auditStatusHistory.page_id ? 'page' : 'script',
+    }))
+};
+
+function* watchPollAuditStatusHistories() {
+    // inspired by https://gist.github.com/ellismarkf/d2824ea9d668c4c00af5112633f91a1d
+    while (true) {
+        const pollAction: ActionType<typeof pollAuditStatusAction> = yield take(pollAuditStatusAction);
+        const auditId = pollAction.payload.auditId;
+        yield race({
+            continuePolling: fork(pollAuditStatusHistories, auditId),
+            stopPolling: take(stopPollingAuditStatusAction)
+        });
+    };
+};
+
 export default function* auditsSaga() {
     yield takeEvery(
         getType(launchAuditAction.request),
         handleAPIExceptions(launchAuditsSaga, launchAuditsFailedHandler),
     );
+    yield takeEvery(
+        getType(stopPollingAuditStatusAction),
+        fetchAuditResultsAfterPolling,
+    );
+    yield watchPollAuditStatusHistories();
 };
