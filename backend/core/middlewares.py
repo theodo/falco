7506bf4ce40bcb02abcad348a6fa43c33181cb96
Guillaume Cornet <guillaumec@theodo.fr>
2019-08-20 17:01:57 +0200

    [422] Update last visit date of user

---
+++ backend/core/middlewares.py
@@ -0,0 +1,29 @@
+import datetime
+from core.models import User
+
+
+class LastVisitedAtUpdateMiddleware:
+    """
+        updates the last_visited_at field in the database for a specific user
+        every time they are authenticated and they load their first project
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        response = self.get_response(request)
+
+        if request.path == "/api/projects/first" and request.user.is_authenticated:
+            # Update last visit time after request finished processing.
+            User.objects.filter(pk=request.user.pk).update(
+                last_visited_at=datetime.datetime.now()
+            )
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
