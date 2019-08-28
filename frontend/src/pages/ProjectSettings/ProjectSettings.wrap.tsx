4eb7a5048d5d017e704fdf583f0bdee5a2080d7b
Guillaume Cornet <guillaumec@theodo.fr>
2019-08-28 11:03:25 +0200

    Hide select list for non admin user & reset select list after adding member to project

--- frontend/src/pages/ProjectSettings/ProjectSettings.wrap.tsx
+++ frontend/src/pages/ProjectSettings/ProjectSettings.wrap.tsx
@@ -5,9 +5,11 @@ import { RootState } from 'redux/types';
 
 import { addMemberToProjectRequest, fetchProjectsRequest } from 'redux/entities/projects';
 import { getProject } from 'redux/entities/projects/selectors';
+import { getUser } from 'redux/user/selectors';
 import ProjectSettings, { OwnProps } from './ProjectSettings';
 
 const mapStateToProps = (state: RootState, props: OwnProps) => ({
+  currentUser: getUser(state),
   project: getProject(state, props.match.params.projectId),
 });
 
