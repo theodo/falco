a7c2b544db29f233238dfd7a00dbe854a4d6e326
Guillaume Cornet <guillaumec@theodo.fr>
2019-08-29 11:52:28 +0200

    [436] Remove members and admins from django admin

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -53,8 +53,7 @@ class ProjectAdmin(admin.ModelAdmin):
         ScriptInline,
         ProjectAuditParametersInline,
     ]
-    exclude = ["screenshot_url"]
-    filter_horizontal = ("members", "admins")
+    exclude = ["screenshot_url", "members", "admins"]
     list_filter = ("is_active",)
     list_display = ("name", "project_actions", "is_active", "created_at")
 
