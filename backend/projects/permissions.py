f0ac8a1e9266f333be922f7b9f10f2b6b1c89457
Louis Pinsard <louisp@theodo.fr>
2019-10-03 12:02:52 +0200

    [447] Send Api key only to admin of project

--- backend/projects/permissions.py
+++ backend/projects/permissions.py
@@ -18,13 +18,22 @@ def check_if_member_of_project(member_id, project_uuid):
 
 
 def check_if_admin_of_project(member_id, project_uuid):
+    if not is_admin_of_project(member_id, project_uuid):
+        raise PermissionDenied(detail="You do not have admin access to this project")
+
+
+def is_admin_of_project(member_id, project_uuid):
     project = Project.objects.get(uuid=project_uuid)
     project_member_role = ProjectMemberRole.objects.filter(
         project_id=project_uuid, user_id=member_id
     )
-
-    if not project_member_role or not project_member_role.first().is_admin:
-        raise PermissionDenied(detail="You do not have admin access to this project")
-
     if not project.is_active:
         raise PermissionDenied(detail="This project has been archived")
+
+    if not project_member_role or not project_member_role.first().is_admin:
+        return False
+
+    if not project.is_active:
+        return False
+
+    return True
