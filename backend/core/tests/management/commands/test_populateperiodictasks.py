5aa93fddc3e89da8d53c67c7a37d630e8e2f195b
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-10-21 17:37:17 +0200

    fix bad copy past

--- backend/core/tests/management/commands/test_populateperiodictasks.py
+++ backend/core/tests/management/commands/test_populateperiodictasks.py
@@ -3,10 +3,10 @@ from django.core.management import call_command
 from django.test import TestCase
 
 
-class ClosepollTest(TestCase):
+class PopulatePeriodicTasksTest(TestCase):
     def test_nb_of_periodicTasks_generated(self):
-        nb_before_command = len(PeriodicTask.objects.all())
+        nb_tasks_before_command = len(PeriodicTask.objects.all())
         call_command("populate-periodic-tasks")
-        nb_after_command = len(PeriodicTask.objects.all())
+        nb_tasks_after_command = len(PeriodicTask.objects.all())
 
-        self.assertEqual(nb_after_command - nb_before_command, 6)
+        self.assertEqual(nb_tasks_after_command - nb_tasks_before_command, 6)
