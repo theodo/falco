e46c3ed3c27e5ae50e347754b04790e9754ef8a1
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-11-07 11:22:41 +0100

    put mock data in separate json files

--- backend/audits/tests/test_tasks.py
+++ backend/audits/tests/test_tasks.py
@@ -1,5 +1,3 @@
-# from django_celery_beat.models import PeriodicTask
-# from django.core.management import call_command
 import httpretty
 import json
 from django.test import TestCase, override_settings
@@ -20,16 +18,17 @@ class TasksTestCase(TestCase):
     @override_settings(CELERY_EAGER=True)
     @patch("audits.tasks.poll_audit_results", poll_audit_results)
     def test_request_audit(self):
-        httpretty.register_uri(
-            httpretty.POST,
-            "https://www.webpagetest.org/runtest.php",
-            body='{ "statusCode": 200, "statusText": "Ok", "data": { "testId": "191024_HA_976b046886025ec8693cbe4f1145929e", "ownerKey": "9259cab7a56de88af4f67e16e631ed954d8c9fee", "jsonUrl": "https://www.webpagetest.org/jsonResult.php?test=191024_HA_976b046886025ec8693cbe4f1145929e", "xmlUrl": "https://www.webpagetest.org/xmlResult/191024_HA_976b046886025ec8693cbe4f1145929e/", "userUrl": "https://www.webpagetest.org/result/191024_HA_976b046886025ec8693cbe4f1145929e/", "summaryCSV": "https://www.webpagetest.org/result/191024_HA_976b046886025ec8693cbe4f1145929e/page_data.csv", "detailCSV": "https://www.webpagetest.org/result/191024_HA_976b046886025ec8693cbe4f1145929e/requests.csv" } }',
+        responseData = json.load(
+            open("audits/tests/json_mocks/wpt_runtest_post_result.json")
         )
-        data = json.load(open("audits/tests/wpt_test.json"))
+        bodyData = open("audits/tests/json_mocks/wpt_runtest_post_body.json").read()
+        httpretty.register_uri(
+            httpretty.POST, "https://www.webpagetest.org/runtest.php", body=bodyData
+        )
         httpretty.register_uri(
             httpretty.GET,
             "https://www.webpagetest.org/jsonResult.php?test=191024_HA_976b046886025ec8693cbe4f1145929e",
-            body=data,
+            body=responseData,
         )
         project = Project.objects.create()
         page_to_audit = Page.objects.create(
