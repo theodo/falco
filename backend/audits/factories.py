9d564a7fecaabd88bbd3e9a544604e9763583651
Nicolas Goutay <nicolas.goutay@gmail.com>
2020-03-19 15:01:52 +0100

    Add factories

---
+++ backend/audits/factories.py
@@ -0,0 +1,61 @@
+import factory
+from . import models
+from projects.factories import PageFactory, ProjectAuditParametersFactory
+
+
+class AuditFactory(factory.Factory):
+    class Meta:
+        model = models.Audit
+
+    page = PageFactory()
+    parameters = ProjectAuditParametersFactory()
+
+
+class AuditStatusHistoryFactory(factory.Factory):
+    class Meta:
+        model = models.AuditStatusHistory
+
+    audit = AuditFactory()
+    status = models.AvailableStatuses.SUCCESS
+
+
+class AuditResultsFactory(factory.Factory):
+    class Meta:
+        model = models.AuditResults
+
+    audit = AuditFactory()
+    wpt_results_json_url = "https://www.webpagetest.org/jsonResult.php?test=200319_SG_5a702cf8c430e2e61bda902f5fd80554&pretty=1"
+    wpt_results_user_url = (
+        "https://www.webpagetest.org/result/200319_SG_5a702cf8c430e2e61bda902f5fd80554/"
+    )
+    wpt_metric_first_view_tti = 2000
+    wpt_metric_repeat_view_tti = 2000
+    wpt_metric_first_view_speed_index = 2000
+    wpt_metric_repeat_view_speed_index = 2000
+    wpt_metric_first_view_first_paint = 2000
+    wpt_metric_repeat_view_first_paint = 2000
+    wpt_metric_first_view_first_meaningful_paint = 2000
+    wpt_metric_repeat_view_first_meaningful_paint = 2000
+    wpt_metric_first_view_load_time = 2000
+    wpt_metric_repeat_view_load_time = 2000
+    wpt_metric_first_view_first_contentful_paint = 2000
+    wpt_metric_repeat_view_first_contentful_paint = 2000
+    wpt_metric_first_view_time_to_first_byte = 2000
+    wpt_metric_repeat_view_time_to_first_byte = 2000
+    wpt_metric_first_view_visually_complete = 2000
+    wpt_metric_repeat_view_visually_complete = 2000
+    wpt_metric_lighthouse_performance = 0.8
+    script_step_name = 2000
+    script_step_number = 2000
+    lh_metric_tti_displayed_value = "2 s"
+    lh_metric_tti_score = 0.8
+    lh_metric_first_contentful_paint_displayed_value = "2 s"
+    lh_metric_first_contentful_paint_score = 0.8
+    lh_metric_speed_index_displayed_value = "2 s"
+    lh_metric_speed_index_score = 0.8
+    lh_metric_first_meaningful_paint_displayed_value = "2 s"
+    lh_metric_first_meaningful_paint_score = 0.8
+    lh_metric_first_cpu_idle_displayed_value = "2 s"
+    lh_metric_first_cpu_idle_score = 0.8
+    lh_metric_max_potential_first_input_delay_displayed_value = "2 s"
+    lh_metric_max_potential_first_input_delay_score = 0.8
--- backend/projects/factories.py
+++ backend/projects/factories.py
@@ -11,3 +11,47 @@ class ProjectFactory(factory.Factory):
     screenshot_url = "https://user-images.githubusercontent.com/2587348/77067173-d548cf00-69e4-11ea-895d-9ac1e90ebab5.png"
     is_active = True
     wpt_instance_url = "https://webpagetest.org"
+
+
+class ProjectMemberRoleFactory(factory.Factory):
+    class Meta:
+        model = models.ProjectMemberRole
+
+
+class PageFactory(factory.Factory):
+    class Meta:
+        model = models.Page
+
+    name = "Homepage"
+    url = "https://getfal.co"
+    project = ProjectFactory()
+
+
+class ScriptFactory(factory.Factory):
+    class Meta:
+        model = models.Script
+
+    name = "User funnel"
+    project = ProjectFactory()
+
+
+class AvailableAuditParametersFactory(factory.Factory):
+    class Meta:
+        model = models.AvailableAuditParameters
+
+    browser = "Chrome"
+    location = "Dulles"
+    location_label = "Dulles, VA"
+    location_group = "Dulles, VA"
+    is_active = True
+    wpt_instance_url = "https://webpagetest.org"
+
+
+class ProjectAuditParametersFactory(factory.Factory):
+    class Meta:
+        model = models.ProjectAuditParameters
+
+    name = "Dulles | Chrome | Cable"
+    configuration = AvailableAuditParametersFactory()
+    network_shape = models.NetworkShapeOptions.CABLE
+    project = ProjectFactory()
