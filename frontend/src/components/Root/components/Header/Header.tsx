e144f03eb97a74703ea50491a9275c1c7437189b
EtienneGrall <44709108+EtienneGrall@users.noreply.github.com>
2020-04-02 14:17:06 +0200

    Make "Docs" and "Admin Interface" clickable again (#171)
    * Fix unclickabe links in account menu
    
    * Fix menu items position

--- frontend/src/components/Root/components/Header/Header.tsx
+++ frontend/src/components/Root/components/Header/Header.tsx
@@ -116,19 +116,19 @@ export const Header: React.FunctionComponent<Props & InjectedIntlProps> = ({
                     <FormattedMessage id="Header.projects_button" />
                   </HeaderButton>
                   <HeaderButtonArrow />
-                  <HeaderMenuItemContent>
-                    {isProjectsMenuVisible && <ProjectsMenu />}
-                  </HeaderMenuItemContent>
                 </HeaderMenuItem>
+                <HeaderMenuItemContent right="270">
+                  {isProjectsMenuVisible && <ProjectsMenu />}
+                </HeaderMenuItemContent>
                 <HeaderMenuItem onClick={toggleAccountMenuVisibility} role="menu">
                   <HeaderButton>
                     <FormattedMessage id="Header.login_button" />
                   </HeaderButton>
                   <HeaderButtonArrow />
-                  <HeaderMenuItemContent>
-                    {isAccountMenuVisible && <AccountMenu />}
-                  </HeaderMenuItemContent>
                 </HeaderMenuItem>
+                <HeaderMenuItemContent right="100">
+                  {isAccountMenuVisible && <AccountMenu />}
+                </HeaderMenuItemContent>
               </HeaderButtonsBlock>
             </Nav>
           ) : (
