a84603ea1d2923024194a5603e1b39f4ed41a29c
MathildeDuboille <duboille.m@gmail.com>
2020-04-03 18:22:52 +0200

    move signals into models file

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django import forms
 from fernet_fields import EncryptedTextField
 from django.contrib.postgres.fields import ArrayField
+from django.db.models.signals import post_save, pre_delete
 
 
 class Project(BaseModel):
@@ -63,6 +64,28 @@ class ProjectMemberRole(BaseModel):
     class Meta:
         ordering = ("-is_admin", "-created_at")
         unique_together = ("project", "user")
+
+
+def save_project_member(sender, instance, **kwargs):
+    MetricsPreferences.objects.create(
+        project=instance.project,
+        user=instance.user,
+        metrics=[
+            "WPTMetricFirstViewTTI",
+            "WPTMetricFirstViewSpeedIndex",
+            "WPTMetricFirstViewLoadTime",
+        ],
+    )
+
+
+def delete_project_member(sender, instance, **kwargs):
+    MetricsPreferences.objects.filter(
+        project=instance.project, user_id=instance.user.id
+    ).delete()
+
+
+post_save.connect(save_project_member, sender=ProjectMemberRole)
+pre_delete.connect(delete_project_member, sender=ProjectMemberRole)
 
 
 class MetricOptions(Enum):
