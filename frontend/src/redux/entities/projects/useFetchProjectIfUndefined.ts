df9c124f1821fb391f875276bc335c0a81890a89
Brendan <Granipouss@gmail.com>
2020-04-23 12:13:10 +0200

    Lint

--- frontend/src/redux/entities/projects/useFetchProjectIfUndefined.ts
+++ frontend/src/redux/entities/projects/useFetchProjectIfUndefined.ts
@@ -1,14 +1,39 @@
-import { useEffect } from "react";
-import { ProjectType } from "./types";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from 'redux/types';
+
+import { fetchProjectsRequest } from './actions';
+import { getProject } from './selectors';
+import { ProjectType } from './types';
 
 export const useFetchProjectIfUndefined = (
-    fetchProjectsRequest: (projectId: string) => void,
-    projectId: string,
-    project?: ProjectType | null,
+  requestFetchProject: (projectId: string) => void,
+  projectId: string,
+  project?: ProjectType | null,
 ) => {
-    useEffect(() => {
-        if (project === undefined) {
-            fetchProjectsRequest(projectId);
-        }
-    }, [fetchProjectsRequest, project, projectId]);
+  useEffect(
+    () => {
+      if (project === undefined) {
+        requestFetchProject(projectId);
+      }
+    },
+    [requestFetchProject, project, projectId],
+  );
+};
+
+export const useProjectById = (projectId: string) => {
+  const project = useSelector((state: RootState) => getProject(state, projectId));
+
+  const dispatch = useDispatch();
+  useEffect(
+    () => {
+      if (project === undefined) {
+        dispatch(fetchProjectsRequest({ currentProjectId: projectId }));
+      }
+    },
+    [dispatch, project, projectId],
+  );
+
+  return project;
 };
