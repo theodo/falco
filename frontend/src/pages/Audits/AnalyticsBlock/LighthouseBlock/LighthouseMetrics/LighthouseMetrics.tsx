d3e44378185e6aa9c308f83b538f5f93da743cc2
Brendan Daoud <Granipouss@gmail.com>
2020-04-28 12:31:41 +0200

    Fix no key warning (#182)
    * Lint
    
    * Add keys

--- frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseMetrics/LighthouseMetrics.tsx
+++ frontend/src/pages/Audits/AnalyticsBlock/LighthouseBlock/LighthouseMetrics/LighthouseMetrics.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { AuditResultType, LighthouseMetricType } from 'redux/auditResults/types';
-import { LIGHTHOUSE_AVERAGE_SCORE_THRESHOLD, LIGHTHOUSE_PASS_SCORE_THRESHOLD } from '../LighthouseScore/LighthouseScore';
+import {
+  LIGHTHOUSE_AVERAGE_SCORE_THRESHOLD,
+  LIGHTHOUSE_PASS_SCORE_THRESHOLD,
+} from '../LighthouseScore/LighthouseScore';
 import {
   Column,
   ColumnsContainer,
@@ -33,7 +36,11 @@ const LighthouseMetrics: React.FunctionComponent<OwnProps> = ({ auditResult }) =
   const PASS = 'PASS';
 
   const metricState = (score: number): 'FAIL' | 'AVERAGE' | 'PASS' => {
-    return score < LIGHTHOUSE_AVERAGE_SCORE_THRESHOLD ? FAIL : score < LIGHTHOUSE_PASS_SCORE_THRESHOLD ? AVERAGE : PASS;
+    return score < LIGHTHOUSE_AVERAGE_SCORE_THRESHOLD
+      ? FAIL
+      : score < LIGHTHOUSE_PASS_SCORE_THRESHOLD
+      ? AVERAGE
+      : PASS;
   };
 
   return (
@@ -42,10 +49,10 @@ const LighthouseMetrics: React.FunctionComponent<OwnProps> = ({ auditResult }) =
         <FormattedMessage id="Audits.lighthouse_header" />
       </MetricsHeader>
       <ColumnsContainer>
-        {metrics.map((column: LighthouseMetricType[]) => (
-          <Column>
+        {metrics.map((column: LighthouseMetricType[], colNum) => (
+          <Column key={colNum}>
             {column.map((metric: LighthouseMetricType) => (
-              <Metric>
+              <Metric key={metric}>
                 <MetricInnerwrap state={metricState(auditResult[metric].score)}>
                   <MetricTitle>
                     <FormattedMessage id={`Metrics.${metric}`} />
