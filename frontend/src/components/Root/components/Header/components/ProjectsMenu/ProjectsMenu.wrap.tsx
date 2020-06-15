9594cde4e46da239daf35af9eff1f415e4c518d3
François Farge <francoisf@theodo.fr>
2020-06-15 18:08:02 +0200

    upgrade react-redux version

--- frontend/src/components/Root/components/Header/components/ProjectsMenu/ProjectsMenu.wrap.tsx
+++ frontend/src/components/Root/components/Header/components/ProjectsMenu/ProjectsMenu.wrap.tsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { getAllProjects } from 'redux/entities/projects/selectors';
 import { getCurrentProject } from 'redux/selectors';
 import { RootStateWithRouter } from 'redux/types';
-import { ProjectsMenu } from './ProjectsMenu';
+import ProjectsMenu from './ProjectsMenu';
 
 const mapStateToProps = (state: RootStateWithRouter) => ({
   currentProject: getCurrentProject(state),
