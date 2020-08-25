c37d5d0182bbe8b9c6a4acd241e13738be55229a
François Farge <fr.farge96@gmail.com>
2020-08-25 17:24:32 +0200

    upgrade docs packages (#222)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,91 +6,92 @@
  */
 
 module.exports = {
-  title: 'Falco · an Open Source WebPageTest runner',
-  tagline: 'Falco helps you monitor, analyze, and optimize your websites.',
-  url: 'https://getfal.co',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'theodo', // Usually your GitHub org/user name.
-  projectName: 'falco', // Usually your repo name.
+  title: "Falco · an Open Source WebPageTest runner",
+  tagline: "Falco helps you monitor, analyze, and optimize your websites.",
+  url: "https://getfal.co",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "theodo", // Usually your GitHub org/user name.
+  projectName: "falco", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Falco',
+      title: "Falco",
       logo: {
-        alt: 'Falco Logo',
-        src: 'img/logo.svg',
+        alt: "Falco Logo",
+        src: "img/logo.svg",
       },
-      links: [
+      items: [
         {
-          to: 'docs/getting-started/installation',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/getting-started/installation",
+          label: "Docs",
+          position: "left",
         },
         {
-          href: 'https://github.com/theodo/falco',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/theodo/falco",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Introduction',
-              to: '/docs/introduction/motivation',
+              label: "Introduction",
+              to: "/docs/introduction/motivation",
             },
             {
-              label: 'Getting Started',
-              to: '/docs/getting-started/installation',
+              label: "Getting Started",
+              to: "/docs/getting-started/installation",
             },
             {
-              label: 'REST API',
-              to: '/docs/api-reference/api-docs',
+              label: "REST API",
+              to: "/docs/api-reference/api-docs",
             },
             {
-              label: 'WebPageTest Private Instances',
-              to: '/docs/wpt-private-instances/about-wpt-private-instances',
+              label: "WebPageTest Private Instances",
+              to: "/docs/wpt-private-instances/about-wpt-private-instances",
             },
           ],
         },
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/theodo/falco',
+              label: "GitHub",
+              href: "https://github.com/theodo/falco",
             },
           ],
         },
       ],
       logo: {
-        alt: 'Theodo Logo',
-        src: 'https://cdn2.hubspot.net/hub/2383597/hubfs/Website/Logos/Logo_Theodo_cropped.svg',
+        alt: "Theodo Logo",
+        src:
+          "https://cdn2.hubspot.net/hub/2383597/hubfs/Website/Logos/Logo_Theodo_cropped.svg",
       },
       copyright: `Copyright © ${new Date().getFullYear()} Theodo. Built with Docusaurus.`,
     },
     algolia: {
-      apiKey: '155e292a0c73c552b48c218cdfc2b4fb',
-      indexName: 'getfal',
+      apiKey: "155e292a0c73c552b48c218cdfc2b4fb",
+      indexName: "getfal",
       algoliaOptions: {}, // Optional, if provided by Algolia
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/theodo/falco/edit/master/docs/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/theodo/falco/edit/master/docs/",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
